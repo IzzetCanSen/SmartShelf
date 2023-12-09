e123f80f178379d5f26460eab3a07fc83cd3a81e
Izzet Can Sen <izzet.sen@student.ehb.be>
2023-12-09 22:20:21 +0100

    Implement shopping list logic

--- screens/ShoppingListScreen.js
+++ screens/ShoppingListScreen.js
@@ -1,10 +1,75 @@
-import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import * as SQLite from "expo-sqlite";
+import { useFocusEffect } from "@react-navigation/native";
 
 const ShoppingListScreen = ({ navigation }) => {
+  const db = SQLite.openDatabase("inventoryDatabase.db");
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  useFocusEffect(
+    React.useCallback(() => {
+      fetchProducts();
+    }, [])
+  );
+
+  const fetchProducts = () => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "SELECT * FROM Product WHERE amount = 0",
+        [],
+        (tx, result) => {
+          const len = result.rows.length;
+          const productsArray = [];
+
+          for (let i = 0; i < len; i++) {
+            const row = result.rows.item(i);
+            productsArray.push({
+              id: row.id,
+              product_name: row.product_name,
+              image_url: row.image_url,
+              brands: row.brands,
+              amount: row.amount,
+            });
+          }
+
+          setProducts(productsArray);
+        },
+        (error) => {
+          console.error("Error fetching products:", error);
+        }
+      );
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>List Screen</Text>
+      <FlatList
+        data={products}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.productItem}>
+            <Text>{item.brands}</Text>
+            <Text>{item.product_name}</Text>
+            <Image
+              style={styles.productImage}
+              source={{ uri: item.image_url }}
+            />
+          </View>
+        )}
+      />
     </View>
   );
 };
@@ -15,9 +80,15 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  text: {
-    fontSize: 20,
-    marginBottom: 20,
+  productItem: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+  },
+  productImage: {
+    width: 200,
+    height: 200,
+    resizeMode: "contain",
   },
 });
 
